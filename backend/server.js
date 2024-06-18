8d74690ec03440c3247d82100a843ad3acd28e31
Utkarsh <utkarshsingh6207603306@gmail.com>
2024-06-18 12:15:39 +0530

    this is the final

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,4 @@
-// import path from "path";
+import path from "path";
 import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
@@ -14,9 +14,12 @@ connectToMongoDB();
 // job.start();
 const app = express();
 const PORT = process.env.PORT || 5000;
+const __dirname = path.resolve();
+const frontendPath = path.resolve(__dirname, '..', 'frontend', 'dist');
 // const __dirname = path.resolve();
 
 // Middlewares
+app.use(express.static(frontendPath))
 app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded({ extended: true })); 
 app.use(cookieParser());
@@ -26,13 +29,17 @@ app.use("/authen/users", userRoutes);
 app.use("/authen/posts", postRoutes);
 
 
+app.get("*", (req, res) => {
+    res.sendFile(path.join(frontendPath, "index.html"))
+})
+
 // if (process.env.NODE_ENV === "production") {
 // 	app.use(express.static(path.join(__dirname, "/frontend/dist")));
 
 // 	// react app
-// 	app.get("*", (req, res) => {
-// 		res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
-// 	});
+	// app.get("*", (req, res) => {
+	// 	res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
+	// });
 // }
 
 app.listen(PORT, () => console.log(`Server started at http://localhost:${PORT}`));
